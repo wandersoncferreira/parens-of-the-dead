8412c3ed064d5d689f91b1a1db3882fe0a1c9c70
Wanderson Ferreira <wand@hey.com>
2021-03-20 22:15:06 -0300

    update after some refactors

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,7 @@
   (cider-default-cljs-repl . shadow-select)
   (cider-preferred-build-tool . clojure-cli)
   (cider-known-endpoints . (("localhost" "8777")))
-  (cider-clojure-cli-global-options . "-A:dev:test")
+  (cider-clojure-cli-global-options . "-A:dev")
   (cider-ns-refresh-after-fn . "user/go")
   (cider-ns-refresh-before-fn . "user/stop")
   (tgt-projects . (((:root-dir "/home/wanderson/arch/study/parens-of-the-dead")
--- dev/user.clj
+++ dev/user.clj
@@ -2,4 +2,4 @@
   (:require [reloaded.repl :refer [system stop reset start go reset-all]]
             [undead.system]))
 
-(reloaded.repl/set-init! #'undead.system/create-system)
+(reloaded.repl/set-init! #'undead.system/create-game)
